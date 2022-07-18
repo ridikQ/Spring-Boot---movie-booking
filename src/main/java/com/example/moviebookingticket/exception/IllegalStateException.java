43dd7a078af40730723c12bfa55fbc298e85185a
iamridik <ridikqendro@gmail.com>
2022-07-18 15:26:25 +0200

    Adding Exception

---
+++ src/main/java/com/example/moviebookingticket/exception/IllegalStateException.java
@@ -0,0 +1,7 @@
+package com.example.moviebookingticket.exception;
+
+public class IllegalStateException extends Exception {
+    public IllegalStateException(String message) {
+        super(message);
+    }
+}
